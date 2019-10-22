ede6174e3d8fc0d01626a673fd76c90cbc198216
Amol Kobal <amol_kobal@rediffmail.com>
2019-10-22 21:46:29 +0530

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SQLiteDemo
+{
+    partial class frmDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTestConnection = new System.Windows.Forms.Button();
+            this.btnAddScore = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnTestConnection
+            // 
+            this.btnTestConnection.Location = new System.Drawing.Point(23, 25);
+            this.btnTestConnection.Name = "btnTestConnection";
+            this.btnTestConnection.Size = new System.Drawing.Size(146, 23);
+            this.btnTestConnection.TabIndex = 0;
+            this.btnTestConnection.Text = "Test Connection";
+            this.btnTestConnection.UseVisualStyleBackColor = true;
+            this.btnTestConnection.Click += new System.EventHandler(this.btnTestConnection_Click);
+            // 
+            // btnAddScore
+            // 
+            this.btnAddScore.Location = new System.Drawing.Point(24, 80);
+            this.btnAddScore.Name = "btnAddScore";
+            this.btnAddScore.Size = new System.Drawing.Size(146, 23);
+            this.btnAddScore.TabIndex = 0;
+            this.btnAddScore.Text = "Add Score";
+            this.btnAddScore.UseVisualStyleBackColor = true;
+            this.btnAddScore.Click += new System.EventHandler(this.btnAddScore_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show Score";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(207, 136);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(367, 181);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Score";
+            // 
+            // frmDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 352);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAddScore);
+            this.Controls.Add(this.btnTestConnection);
+            this.Name = "frmDemo";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTestConnection;
+        private System.Windows.Forms.Button btnAddScore;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace SQLiteDemo
+{
+    public partial class frmDemo : Form
+    {
+        public frmDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestConnection_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SQLiteConnection.CreateFile("MyDatabase.sqlite");
+
+                SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+                m_dbConnection.Open();
+
+                string sql = "if exists(select * from highscores)";
+
+                sql = "create table highscores (name varchar(20), score int)";
+                
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                command.ExecuteNonQuery();
+
+                sql = "insert into highscores (name, score) values ('Me', 9001)";
+
+                command = new SQLiteCommand(sql, m_dbConnection);
+                command.ExecuteNonQuery();
+
+                m_dbConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnAddScore_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+            string sql = "insert into highscores (name, score) values ('You', 9002)";
+
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            m_dbConnection.Close();
+
+            MessageBox.Show("Added");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+
+            string sql = "select * from highscores";
+
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(sql, m_dbConnection);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+
+            if (ds.Tables.Count > 0)
+            {
+                for (int index = 0; index < ds.Tables[0].Rows.Count; index++)
+                {
+                    ListViewItem item = listView1.Items.Add(new ListViewItem(ds.Tables[0].Rows[index]["name"].ToString()));
+                    item.SubItems.Add(ds.Tables[0].Rows[index]["score"].ToString());
+                }
+            }
+        }
+    }
+}
